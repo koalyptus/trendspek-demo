cfa4e140ccfc4dc0f78ad2ab9c3b323cbfecdec8
koalyptus <maxgug@hotmail.com>
2026-09-24 08:11:35 +1000

    miscellaneous refinements (1)

--- src/database/migrations/annotation.migration.1.ts
+++ src/database/migrations/annotation.migration.1.ts
@@ -1,4 +1,4 @@
 import type { RxDocumentData } from 'rxdb'
 import type { DefectAnnotation } from '@/types'
 
-export function annotationMigration1(oldDoc:  RxDocumentData<DefectAnnotation> | null) { return oldDoc } 
+export function annotationMigration1(oldDoc:  RxDocumentData<DefectAnnotation> | null) { return oldDoc }
